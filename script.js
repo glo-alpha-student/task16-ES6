5cb4e97ac48c3de65638014aec96d7a86a718bee
aismeg <74493829+aismeg@users.noreply.github.com>
2022-11-10 23:24:22 +0300

    Update
    .

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 'use strict';
 
 class First {
-
-    static hello() {
+    hello() {
         console.log('Привет я метод родителя!');
     }
-
 }
 class Second extends First {
-
     hello() {
-        First.hello();
         console.log('А я наследуемый метод!');
-  }
-
+    }
 }
+const family = [new First(), new Second()];
 
-new Second().hello();
-
+family.forEach((message) => {
+    message.hello();
+});
